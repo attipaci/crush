4bb73bb6c86edddff03c18ee446a687b01f99941
Attila Kovacs <attipaci@gmail.com>
2013-11-25 16:57:03 +0000

    2.15-2b2

--- crush/src/crush/CRUSH.java
+++ crush/src/crush/CRUSH.java
@@ -47,7 +47,7 @@ public class CRUSH extends Configurator {
 	private static final long serialVersionUID = 6284421525275783456L;
 	
 	private static String version = "2.15-2";
-	private static String revision = "beta";
+	private static String revision = "beta2";
 	public static String workPath = ".";
 	public static String home = ".";
 	public static boolean debug = false;
