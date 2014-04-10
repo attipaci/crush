893f18277ed109cb864da9241dc5e3a443859efe
micimacko <micimacko@c7240f7d-c713-4240-b6c4-36d55466b176>
2014-04-10 20:26:45 +0000

    2.16-a6
    - projections bug fix (util.jar)

--- crush/src/crush/CRUSH.java
+++ crush/src/crush/CRUSH.java
@@ -36,7 +36,7 @@ import nom.tam.util.*;
 /**
  * 
  * @author Attila Kovacs
- * @version 2.16-a4
+ * @version 2.16-a6
  * 
  */
 public class CRUSH extends Configurator {
@@ -45,8 +45,8 @@ public class CRUSH extends Configurator {
 	 */
 	private static final long serialVersionUID = 6284421525275783456L;
 	
-	private static String version = "2.16-a4";
-	private static String revision = "alpha.4";
+	private static String version = "2.16-a6";
+	private static String revision = "alpha.6";
 	public static String workPath = ".";
 	public static String home = ".";
 	public static boolean debug = false;
