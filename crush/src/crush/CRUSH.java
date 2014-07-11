58108169c70682c3c22eae60034d00372f05a497
Attila Kovacs <attipaci@gmail.com>
2014-07-11 01:31:47 +0000

    2.20-b1
    * crushtools ready and supercharged!
    * FITS 1.11
    * Parallelized some FITS reading integration preprocessing tasks.

--- crush/src/crush/CRUSH.java
+++ crush/src/crush/CRUSH.java
@@ -36,7 +36,7 @@ import nom.tam.util.*;
 /**
  * 
  * @author Attila Kovacs
- * @version 2.17-b2
+ * @version 2.20-b1
  * 
  */
 public class CRUSH extends Configurator {
@@ -45,7 +45,7 @@ public class CRUSH extends Configurator {
 	 */
 	private static final long serialVersionUID = 6284421525275783456L;
 	
-	private static String version = "2.17-b2";
+	private static String version = "2.20-b1";
 	private static String revision = "beta";
 	public static String workPath = ".";
 	public static String home = ".";
