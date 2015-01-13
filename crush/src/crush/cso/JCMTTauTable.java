004f2655b6d0fd4c2e31d989d119d3f2dcdaa821
micimacko <micimacko@c7240f7d-c713-4240-b6c4-36d55466b176>
2015-01-13 16:11:45 +0000

    2.21-1 (final)

--- crush/src/crush/cso/JCMTTauTable.java
+++ crush/src/crush/cso/JCMTTauTable.java
@@ -25,8 +25,12 @@ package crush.cso;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.StringTokenizer;
@@ -68,9 +72,25 @@ public class JCMTTauTable  extends LocalAverage<JCMTTauTable.Entry> {
 	
 	protected void read(int iMJD, String fileName) throws IOException {
 		System.err.print("   [Loading tau data] ");
+			
+		BufferedReader in = null;
 		
-		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		try { 
+			in = getFileReader(fileName); 
+			System.err.print("-- local: "); 
+		}
+		catch(IOException e) {		
+			if(!fileName.contains("://")) fileName = "http://" + fileName;
+			try { 
+				in = getURLReader(fileName); 
+				System.err.print("-- URL: "); 
+			}
+			catch(SocketTimeoutException e2) { System.err.println("WARNING! JCMT tau table timeout."); }
+			catch(IOException e2) { System.err.println("WARNING! JCMT tau tables error: " + fileName); }
+		}
+		if(in == null) throw new FileNotFoundException(fileName);
 		
+		// Skip the header line...
 		String line = in.readLine();
 		
 		while((line = in.readLine()) != null) if(line.length() > 0) if(line.charAt(0) != '#') if(line.charAt(0) != ' ') {
@@ -84,7 +104,7 @@ public class JCMTTauTable  extends LocalAverage<JCMTTauTable.Entry> {
 		}
 		in.close();
 		
-		System.err.println("-- " + size() + " values parsed.");
+		System.err.println(size() + " values parsed.");
 		
 		Collections.sort(this);
 		
@@ -93,11 +113,26 @@ public class JCMTTauTable  extends LocalAverage<JCMTTauTable.Entry> {
 		
 	}	
 	
+	
+	private BufferedReader getFileReader(String fileName) throws IOException {
+		return new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+	}
+	
+	private BufferedReader getURLReader(String address) throws IOException {
+		URL versionURL = new URL(address);
+		URLConnection connection = versionURL.openConnection();
+		
+		connection.setConnectTimeout(3000);
+		connection.setReadTimeout(2000);
+		connection.connect();
+		return new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+	}
+		
+	
 	public double getTau(double MJD) {	
 		Entry mean = getLocalAverage(new TimeStamp(MJD));
-		
-		
-		
+			
 		if(mean.tau.weight() == 0.0) {
 			System.err.println("   ... No skydip data was found in specified time window.");
 			
