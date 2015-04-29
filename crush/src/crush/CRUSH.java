a881c3d78786c73117724227d6c908514d239459
micimacko <micimacko@c7240f7d-c713-4240-b6c4-36d55466b176>
2015-04-29 16:48:47 +0000

    2.23-b4

--- crush/src/crush/CRUSH.java
+++ crush/src/crush/CRUSH.java
@@ -36,7 +36,7 @@ import nom.tam.util.*;
 /**
  * 
  * @author Attila Kovacs
- * @version 2.23-1
+ * @version 2.23-b4
  * 
  */
 public class CRUSH extends Configurator {
@@ -45,8 +45,8 @@ public class CRUSH extends Configurator {
 	 */
 	private static final long serialVersionUID = 6284421525275783456L;
 	
-	private static String version = "2.23-1";
-	private static String revision = "devel.1";
+	private static String version = "2.23-b4";
+	private static String revision = "beta";
 	public static String workPath = ".";
 	public static String home = ".";
 	public static boolean debug = false;
