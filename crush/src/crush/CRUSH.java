d886099f677292361b5603725a74dfad39387495
Attila Kovacs <attipaci@gmail.com>
2019-07-10 13:33:06 +0200

    2.50-1: version updated to stable release.

--- crush/src/crush/CRUSH.java
+++ crush/src/crush/CRUSH.java
@@ -60,8 +60,8 @@ public class CRUSH extends Configurator implements BasicMessaging {
      */
     private static final long serialVersionUID = 6284421525275783456L;
 
-    private final static String version = "2.50-b2";
-    private final static String revision = "devel.2";
+    private final static String version = "2.50-1";
+    private final static String revision = "";
 
     public static String home = ".";
     public static boolean debug = false;
